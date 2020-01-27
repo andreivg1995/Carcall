91b4f6226518a3e1e0b506699be49c0473401596
Andrei <andrei.gindac@in2.es>
2020-01-27 11:39:38 +0100

    Current location

--- app/src/main/java/com/example/carcall/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/carcall/ui/home/HomeFragment.java
@@ -1,36 +1,46 @@
 package com.example.carcall.ui.home;
 
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.carcall.MainActivity;
 import com.example.carcall.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import static androidx.core.app.ActivityCompat.*;
 
 public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
     private HomeViewModel homeViewModel;
 
-    LocationManager locationManager;
-    LocationListener locationListener;
-    LatLng userLatLong;
-
     private GoogleMap mMap;
+
+    private Location currentLocation;
+    private FusedLocationProviderClient fusedLocationClient;
+    private static final int REQUEST_CODE = 101;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -46,18 +56,63 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
         });
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used
+        /*
         SupportMapFragment mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapa);
         mMapFragment.getMapAsync(this);
+        */
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
+        fetchLastLocation();
         return root;
+    }
+
+    private void fetchLastLocation() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(getActivity(), new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+
+        }
+        Task<Location> task = fusedLocationClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null){
+                    currentLocation = location;
+                    Toast.makeText(getContext(), currentLocation.getLatitude() + "" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    SupportMapFragment mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapa);
+                    mMapFragment.getMapAsync((OnMapReadyCallback) getContext());
+                }
+            }
+        });
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(latLng).title("I'am here"));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
         // Marker in Sydney
+        /*
         LatLng latLng = new LatLng(-34,151);
         mMap.addMarker(new MarkerOptions().position(latLng).title("Sydney"));
         googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        */
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    fetchLastLocation();
+                }
+                break;
+        }
     }
 }
