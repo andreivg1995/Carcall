a683cc6adf77d99c39d2f967ef4dd13b72df4541
andreivg <cf17andrei.gindac@iesjoandaustria.org>
2020-01-30 18:04:00 +0100

    Livedata firebase query user viaje

--- app/src/main/java/com/example/carcall/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/carcall/ui/gallery/GalleryFragment.java
@@ -16,6 +16,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.carcall.R;
+import com.example.carcall.Viaje;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class GalleryFragment extends Fragment {
 
@@ -23,6 +30,9 @@ public class GalleryFragment extends Fragment {
 
     private RecyclerView recyclerView;
     private AdapterViajes adapter;
+
+    FirebaseAuth mAuth;
+    FirebaseRecyclerOptions<Viaje> options;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -33,15 +43,41 @@ public class GalleryFragment extends Fragment {
         recyclerView = root.findViewById(R.id.recyclerMisViajes);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        galleryViewModel.firebaseQuery(user);
+
+        galleryViewModel.getViajes().observe(this, new Observer<Query>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onChanged(Query query) {
+                options =
+                        new FirebaseRecyclerOptions.Builder<Viaje>()
+                                .setQuery(query, Viaje.class)
+                                .build();
+
+                adapter = new AdapterViajes(options);
+                recyclerView.setAdapter(adapter);
+                adapter.startListening();
             }
         });
         return root;
     }
+
+    /*
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+    */
 
     public class AdapterViajes extends FirebaseRecyclerAdapter<Viaje, AdapterViajes.ViewHolder> {
         /**
@@ -57,7 +93,7 @@ public class GalleryFragment extends Fragment {
         @Override
         protected void onBindViewHolder(@NonNull AdapterViajes.ViewHolder holder, int position, @NonNull Viaje model) {
             // Bind the Chat object to the ChatHolder
-            holder.tvCalleSalida.setText(model.getMensaje());
+            holder.tvCalleSalida.setText(model.getcSalida());
         }
 
         @NonNull
--- app/src/main/java/com/example/carcall/ui/gallery/GalleryViewModel.java
+++ app/src/main/java/com/example/carcall/ui/gallery/GalleryViewModel.java
@@ -4,16 +4,30 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
 public class GalleryViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<Query> mQuery;
 
     public GalleryViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mQuery = new MutableLiveData<>();
+        //mText.setValue("This is gallery fragment");
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<Query> getViajes() {
+        return mQuery;
+    }
+
+    public void firebaseQuery(FirebaseUser user) {
+        Query query = FirebaseDatabase.getInstance()
+                .getReference()
+                .child("viajes")
+                .orderByChild("uid")
+                .equalTo(user.getUid());
+
+        mQuery.postValue(query);
     }
 }
