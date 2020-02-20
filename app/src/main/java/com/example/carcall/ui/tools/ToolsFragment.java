e01eb92bc84fb6f1395713b832a8471971eff1f6
andreivg <cf17andrei.gindac@iesjoandaustria.org>
2020-02-20 16:05:35 +0100

    Take photo and upload to Firebase

--- app/src/main/java/com/example/carcall/ui/tools/ToolsFragment.java
+++ app/src/main/java/com/example/carcall/ui/tools/ToolsFragment.java
@@ -1,10 +1,21 @@
 package com.example.carcall.ui.tools;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -13,10 +24,25 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.carcall.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ToolsFragment extends Fragment {
 
     private ToolsViewModel toolsViewModel;
+
+    ImageView image;
+    ImageButton bUpload, bFoto;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    Bitmap imageBitmap = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +56,79 @@ public class ToolsFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        image = root.findViewById(R.id.imageView);
+        bUpload = root.findViewById(R.id.uploadImageButton);
+        bFoto = root.findViewById(R.id.fotoImageButton);
+
+        bFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                take_photo();
+            }
+        });
+
+        bUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fileUploader();
+            }
+        });
+
         return root;
+    }
+
+    private void take_photo() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+
+        }
+
+        if (imageBitmap != null) {
+            image.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void fileUploader() {
+
+        // Get the data from an ImageView as bytes
+        image.setDrawingCacheEnabled(true);
+        image.buildDrawingCache();
+        Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        // Create a storage reference from our app
+        StorageReference storageRef = storage.getReference();
+        // Create a reference to "mountains.jpg"
+        StorageReference mountainsRef = storageRef.child("test.jpg");
+
+        UploadTask uploadTask = mountainsRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+                Toast.makeText(getContext(), "Image not uploaded", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                Toast.makeText(getContext(), "Image uploaded", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
